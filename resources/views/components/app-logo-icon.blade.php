751b3a1a1c1f82df75a5456c5d6715bd150071fb
Leonardo Maldonado <lrmaldo@gmail.com>
2025-09-17 11:07:30 -0600

    Actualizar la forma de cargar la imagen del logo utilizando la función asset() en lugar de url()

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,4 +1,4 @@
 @props([
     'alt' => config('app.name', 'IXXI TECNOLOGÍA'),
 ])
-<img src="{{ url('img/logo.png') }}" alt="{{ $alt }}" loading="lazy" {{ $attributes->merge(['class' => 'block h-auto w-auto max-h-10 select-none']) }} />
+<img src="{{ asset('img/logo.png') }}" alt="{{ $alt }}" loading="lazy" {{ $attributes->merge(['class' => 'block h-auto w-auto max-h-10 select-none']) }} />
